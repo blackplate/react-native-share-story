00c403e31f3d7db1bd4e6b33f38224ca497970ca
dawidvdh <hello@dawid.codes>
2022-02-26 15:22:47 +0100

    build: update example project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  quoteProps: 'consistent',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { NativeModules, Platform } from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-share-story' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
